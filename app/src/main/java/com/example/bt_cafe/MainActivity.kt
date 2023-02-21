46dfd35dc5411a390f7c0e07a69d6016e252c7c7
lagwalsahil21821 <lagwalsahil7978@gmail.com>
2023-02-21 09:28:08 +0530

    use of modifier to apply bg color, height, padding in compose

--- app/src/main/java/com/example/bt_cafe/MainActivity.kt
+++ app/src/main/java/com/example/bt_cafe/MainActivity.kt
@@ -3,66 +3,98 @@ package com.example.bt_cafe
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Gray
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.bt_cafe.ui.theme.BT_CafeTheme
+import org.intellij.lang.annotations.JdkConstants
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            /*BT_CafeTheme {
-                // A surface container using the 'background' color from the theme
+            BT_CafeTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    RestaurantName(
-                        name = stringResource(id = R.string.title),
-                        size = 32
-                    )
-                }
-            }*/
-
-            // layout: column, row, box
-            Column() {
-                Text(
-                    text = stringResource(id = R.string.title),
-                    color = Color(0xFFF4CE14),
-                    fontSize = 32.sp
-                )
-
-                Text(
-                    text = stringResource(id = R.string.location),
-                    color = Color(0xFFF4CE14)
-                )
-
-                Row() {
-                    Button(onClick = { /*TODO*/ }) {
-                        Text(text = stringResource(id = R.string.order))
+                    color = MaterialTheme.colors.background) {
+                        UpperPart()
                     }
-                    Image(painter = painterResource(id = R.drawable.cafe), contentDescription = "")
-                }
             }
+
+
+
 
 
         }
     }
 }
 
+@Composable
+fun UpperPart(){
+    // layout: column, row, box
+    Column(
+        Modifier
+            .fillMaxSize()
+            .background(Color(0xFF495E57)),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.Start
+    ) {
+        Text(
+            text = stringResource(id = R.string.title),
+            color = Color(0xFFF4CE14),
+            fontSize = 32.sp,
+            modifier = Modifier.padding(start = 20.dp, top = 20.dp)
+        )
+
+        Text(
+            text = stringResource(id = R.string.location),
+            fontSize = 24.sp,
+            color = Color(0xFFFFFFFF),
+            modifier = Modifier.padding(start = 20.dp)
+        )
+
+        Row(
+            Modifier.fillMaxSize(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Button(
+                onClick = { /*TODO*/ },
+                border = BorderStroke(1.dp, Color.Red),
+                shape = RoundedCornerShape(10.dp),
+                colors = ButtonDefaults.buttonColors(backgroundColor = Color.Gray)
+            ) {
+                Text(text = stringResource(id = R.string.order))
+            }
+
+            Image(
+                painter = painterResource(id = R.drawable.cafe),
+                contentDescription = "",
+                Modifier.height(100.dp)
+            )
+
+
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun UpperPartPreview(){
+    UpperPart()
+}
 
 /*
 @Composable
